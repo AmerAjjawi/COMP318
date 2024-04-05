ce0652d9a4c6f0dc2f5004c88c44ea06eabc1869
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2024-04-04 21:48:08 -0400

    grabbed raw data and transformed it.

--- nltk.py
+++ nltk.py
@@ -17,3 +17,24 @@ with urllib.request.urlopen("https://www.canada.ca/en/health-canada/corporate/ab
     canLaw = url.read()
 
 print(canLaw)
+
+#tokenize and split the strings
+token_split = canLaw.split()
+import pprint 
+#printing the first 400 words
+pprint.pprint(token_split[:400])
+#convert from binary to string
+import re
+canLaw = str(canLaw, 'utf-8')
+#grab the raw data and split it
+token_with_re = re.split(r'\W+', canLaw)
+#printing the length
+print("len(token_with_re)")
+print(len(token_with_re))
+print("up to 600 tokens from token_with_re")
+print(token_with_re[:600])
+import pprint
+pprint.pprint(token_with_re[:600])
+
+#fix add remove of alphanumerical
+#clean_text = [re.sub(r'[^0-9a-zA-Z\s]+', '', token_with_re).lower() for token in token_with_re if token.strip() != " "] 
