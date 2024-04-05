a32ff9b321a9616d6b6efa29ec9300b520f3d65e
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2024-04-04 21:16:03 -0400

    request url and read it

--- nltk.py
+++ nltk.py
@@ -5,8 +5,15 @@ import seaborn as sn
 from bs4 import BeautifulSoup 
 import random
 from wordcloud import wordcloud
+import os
+import spacy 
+nlp = spacy.load('en_core_web_sm') #assigns specific token vectors
+
 import urllib as req 
 
-#setting up the url for canadian law
+#request url for canadian law
 resp = req.urlopen("https://www.canada.ca/en/health-canada/corporate/about-health-canada/legislation-guidelines/acts-regulations/list-acts-regulations.html") #this is the response object
 
+#read the webpage
+canadian_law = resp.read()
+print(canadian_law)
