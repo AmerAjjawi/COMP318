0073dc1c0465140b0d59f8402f5e31d6eb88d856
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2024-04-04 21:02:00 -0400

    added the proper libaries

--- nltk.py
+++ nltk.py
@@ -1,2 +1,12 @@
-#import nltk, numpy, pandas, seaborn, beautiful soup
+#import libaries
 import nltk
+import pandas as pd
+import seaborn as sn 
+from bs4 import BeautifulSoup 
+import random
+from wordcloud import wordcloud
+import urllib as req 
+
+#setting up the url for canadian law
+resp = req.urlopen("https://www.canada.ca/en/health-canada/corporate/about-health-canada/legislation-guidelines/acts-regulations/list-acts-regulations.html") #this is the response object
+
